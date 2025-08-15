682c2e89450443b57e93cdb863b627755ba4dfa9
AlejandroZBarajas <falejandrozb@gmail.com>
2025-08-15 09:01:18 -0600

    plantilla login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 // src/App.jsx
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-2xl shadow-lg max-w-sm text-center">
-        <h1 className="text-3xl font-bold text-blue-600 mb-4">
-          ¡Tailwind está funcionando! 🎉
+    <div className="flex flex-col h-screen bg-white  items-center  ">
+      <div className="w-full flex flex-col justify-center items-center bg-morado items-center h-1/5">
+        <h1 className="text-4xl font-black text-white mb-4 border-solid text-center">
+          Gestión de espacios educativos
         </h1>
-        <p className="text-gray-700 mb-6">
-          Si ves este texto con estilos bonitos, TailwindCSS está listo.
-        </p>
-        <div className="flex gap-4 justify-center">
-          <div className="bg-green-500 text-white px-4 py-2 rounded-lg">Botón 1</div>
-          <div className="bg-red-500 text-white px-4 py-2 rounded-lg">Botón 2</div>
+      </div>
+
+    <div className="flex flex-1 w-full  justify-center items-center">
+      
+      <div className="flex flex-col justify-center bg-gris border-2 border-solid border-morado border-2 p-8 rounded-lg shadow-lg max-w-sm text-center h-3/5 max-w-1/4 min-w-[300px] justify-evenl">
+        <h2 className="text-morado text-3xl font-bold m-2">E-mail</h2>
+        <input type="email" className="border-2 border-solid border-morado bg-gris-clarito rounded-lg mb-4" />
+        <h2 className="text-3xl text-morado font-bold m-2">Contraseña</h2>
+        <input type="email" className="border-2 border-solid border-morado bg-gris-clarito rounded-lg mb-4" />
+        <div className="bg-morado rounded-4xl">
+          <h2 className="text-3xl text-white font-black p-2">Ingresar</h2>
         </div>
+      </div>
+
+
       </div>
     </div>
   );
